0a5ab498c5e00c21ca69aff27263d0040eb56798
Massaru <massaruhagiopro@gmail.com>
2022-10-15 12:57:25 +0900

    criando logica

---
+++ script.js
@@ -0,0 +1,19 @@
+const FROM = "card_from";
+const BACK = "card_back";
+
+let techs = [
+    "bootstrap",
+    "css3",
+    "electron",
+    "firebase",
+    "html5",
+    "jquery",
+    "jss",
+    "linuxmint",
+    "mongodb",
+    "nodedotjs",
+];
+
+function creatCardsFromTechs(techs){
+    
+}
